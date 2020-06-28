0ca56b572154a0bd17f94dd2013a2ec4159e0961
Bowen Chen <chen_bowen@bowens-mbp.lan>
2020-06-28 12:28:41 -0700

    fix word frequencies and word tokenizers

--- sentiment_analysis/features/word_frequencies.py
+++ sentiment_analysis/features/word_frequencies.py
@@ -1,6 +1,7 @@
 from sklearn.base import BaseEstimator
 from sentiment_analysis.data.review_processor import ReviewProcessor
 from sentiment_analysis.utils.word_tokenizer import WordTokenizer
+from collections import Counter
 import pandas as pd
 import numpy as np
 
@@ -14,6 +15,8 @@ class WordFrequencyVectorizer(BaseEstimator):
         option="all",
     ):
         self.tokenizer = WordTokenizer()
+        self.categories = categories
+        self.option = option
         self.set_word_index_mapping()
 
     def set_word_index_mapping(self):
@@ -21,25 +24,28 @@ class WordFrequencyVectorizer(BaseEstimator):
         self.word_to_index_map = processed_review.word_to_index_map
         self.vocab_size = processed_review.vocab_size
 
-    @staticmethod
-    def get_word_frequency_vector(review_text, word_to_index_map):
+    def get_word_frequency_vector(self, review_text):
         """ Get the word frequency vector for one tokenized review"""
         # get the tokenized review from the review text
         tokenized_review = self.tokenizer.tokenize_sentence(review_text)
 
+        # replace the unknown word with unknown_word token
+        tokenized_review_processed = [
+            word if word in list(self.word_to_index_map) else "unknown_word"
+            for word in tokenized_review
+        ]
+
         # get count for all words appeared in the tokenized review
-        word_frequency_count = Counter(tokenized_review)
+        word_frequency_count = Counter(tokenized_review_processed)
 
         # map all the words to indices using the word to index map
         word_frequency_count_ind = {
-            word_to_index_map[
-                word if word in list(word_to_index_map.keys()) else "unknown_word"
-            ]: count
+            self.word_to_index_map[word]: count
             for word, count in word_frequency_count.items()
         }
 
         # flatten to a vector that equals to the vocabulary size + 1 (last index for unknown word)
-        word_frequency_vector = np.zeros(len(word_to_index_map) + 1)
+        word_frequency_vector = np.zeros(len(self.word_to_index_map) + 1)
         for ind, count in word_frequency_count_ind.items():
             word_frequency_vector[ind] += count
 
@@ -50,15 +56,6 @@ class WordFrequencyVectorizer(BaseEstimator):
 
     def transform(self, X, y=None):
         """ Get the word frequency vectors for all tokenized reviews """
-        word_frequency_vectors = list(
-            map(
-                partial(
-                    self.get_word_frequency_vector,
-                    word_to_index_map=self.word_to_index_map,
-                ),
-                X,
-            )
-        )
-
+        word_frequency_vectors = list(map(self.get_word_frequency_vector, X))
         word_frequency_matrix = np.stack(word_frequency_vectors, axis=0)
         return word_frequency_matrix
--- sentiment_analysis/utils/word_tokenizer.py
+++ sentiment_analysis/utils/word_tokenizer.py
@@ -1,3 +1,4 @@
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
